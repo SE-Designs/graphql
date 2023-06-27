3a76832b2cc1f930240a8498171ad4a128290725
mixture <dragonlorefnstattrack@gmail.com>
2023-06-27 11:41:01 +0600

    upd: in-memory cache hotfix

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
+import AddClientsModal from "./components/AddClientsModal";
 import Clients from "./components/Clients";
 import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: "http://localhost:8080/graphql",
-  cache: new InMemoryCache(),
+  cache,
 });
 
 function App() {
@@ -13,6 +33,7 @@ function App() {
       <ApolloProvider client={client}>
         <Header />
         <div className="container">
+          <AddClientsModal />
           <Clients />
         </div>
       </ApolloProvider>
