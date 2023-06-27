904ecdcfd62c1f76619bff30d674a18ab00c9970
mixture <dragonlorefnstattrack@gmail.com>
2023-06-27 11:41:37 +0600

    upd: apollo client mutations

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,6 +1,27 @@
+import { useMutation } from "@apollo/client";
 import { FaTrash } from "react-icons/fa";
+import { GET_CLIENTS } from "../queries/clientQueries";
+import { DELETE_CLIENT } from "../mutations/clientMutations";
 
 function ClientRow({ client }) {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: {
+      id: client.id,
+    },
+    update(cache, { data: { deleteClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: {
+          clients: clients.filter((client) => client.id !== deleteClient.id),
+        },
+      });
+    },
+
+    // refetchQueries: [{ query: GET_CLIENTS }],
+  });
+
   return (
     <>
       <tr>
@@ -8,7 +29,7 @@ function ClientRow({ client }) {
         <td>{client.email}</td>
         <td>{client.phone}</td>
         <td>
-          <button className="btn btn-danger btn-sm">
+          <button onClick={deleteClient} className="btn btn-danger btn-sm">
             <FaTrash />
           </button>
         </td>
